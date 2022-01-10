88bf90857cd1f2981976a815e6cb819fe2089452
kevinrolanduniverse <67627719+kevinrolanduniverse@users.noreply.github.com>
2022-01-10 19:25:31 +0530

    Update connection.php

--- file/connection.php
+++ file/connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bloodbank";
+$servername = "127.0.0.1:53263";
+$username = "azure";
+$password = "6#vWHD_$";
+$dbname = "localdb";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if(!$conn){
  die('Could not Connect MySql:' .mysql_error());
